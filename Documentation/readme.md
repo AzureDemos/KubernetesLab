ba56fa456e34454909fd7f6c377cb2ae399f55ca
Steve Leonard <steve-leonard@outlook.com>
2018-09-20 19:19:45 +0100

    yaml

--- Documentation/readme.md
+++ Documentation/readme.md
@@ -1,4 +1,49 @@
 
+## Setup Docker for Windows
+
+Install Docker for Windows - link
+
+Enable Kubernetes
+
+![Enable Local Kubernetes](images/enablekubernetesondockerwindows.png)
+
+![Switch Kubernetes Context](images/switchkubernetescontext.png)
+
+### Alternativly, via the CLI 
+
+You can view all contexts
+```
+kubectl config view
+```
+
+View you current context
+
+```
+kubectl config current-context  
+```
+
+Switch your current context
+
+```
+kubectl config use-context context-name-here
+```
+
+## Viewing you local dashboard
+
+Firstly make sure you have installed the dashboard UI
+
+```
+kubectl create -f https://raw.githubusercontent.com/kubernetes/dashboard/master/src/deploy/recommended/kubernetes-dashboard.yaml
+```
+
+Now setup a proxy
+
+```
+kubectl proxy
+```
+
+Your dashboard will be made available here: http://localhost:8001/api/v1/namespaces/kube-system/services/https:kubernetes-dashboard:/proxy/
+
 ## Setting up our environment using the Azure CLI
 
 ### Create resource group
@@ -21,9 +66,41 @@ az group create --name acrDemo --location westeurope
 az acr create --resource-group acrDemo --name stevesACR --sku Basic
 ```
 
+### Create an Azure Container Registry
 
+It may be worthwhile putting the registry in a different resource group to the cluster
+```
+az group create --name acrDemo --location westeurope
+az acr create --resource-group acrDemo --name azureDemosACR --sku Basic
+```
 
 ## Create Azure DevOps Project
 ![Create DevOps Project](images/createproject.png)
 
+## Import Repo
+![Create DevOps Project](images/importrepo.png)
 
+New build pipeline
+
+![Create DevOps Project](images/newbuildpipeline.png)
+
+Create empty piple line
+
+![](images/emptybuild.png)
+
+linux agent - name it
+
+![](images/linuxagentbuild.png)
+
+add docker build step
+
+![](images/adddockerbuildstep.png)
+
+
+API build step
+
+![](images/apibuild.png)
+
+Push API Build
+
+![](images/pushapibuild.png)
---
+++ Source/YAML/scripts.sh
@@ -0,0 +1,76 @@
+
+GET KUBE CONFIG
+az aks get-credentials -g aks -n slaks -f - > .kube\config
+Then open the editor in the cloud shell by clickuing the icon - opens as VS CODE editor 
+
+kubectl create secret docker-registry regsecret --docker-server=stevescontainers.azurecr.io --docker-username=stevescontainers --docker-password=J2G7qvJdcidy6UhWWROH=pPU1Tjb6UQj --docker-email=stleonar@micrososft.com
+
+
+
+http://default-frontend-service2.55474723f3b7466899bc.westeurope.aksapp.io
+
+http://dev-sampleapp.4f02b96afa9f44b084dc.northeurope.aksapp.io/
+
+
+
+App Settings
+--------------------------------------------------------------------------------------
+Type        FileName                    Values                      Location
+--------------------------------------------------------------------------------------
+Env Vars    - NA                        - EnvironmentName           NA
+Internal    - appsettings.json          - App:CodeVersion           AppRoot
+ConfigMap   - configmap-frontend.xml    - App:Theme                 AppRoot/configs
+ConfigMap   - configmap-frontend.json   - API:URI                   AppRoot/configs
+Secret      - secret-frontend.json      - API:Key, API:Secret      AppRoot/secrets
+
+#Environment variable for backend api
+BACKEND_SERVICE_PORT
+
+*****************************************************************************
+
+#get creds
+az aks get-credentials --resource-group aksDemo --name aksCluster
+
+#browse dashboard
+az aks browse --resource-group aksDemo --name aksCluster
+
+#go to correct folder
+cd C:\Users\stleonar\Documents\GitHub\KubernetesLab\Source\YAML
+
+#create namespace 
+kubectl create -f namespace-dev.yaml
+kubectl create -f namespace-prod.yaml
+
+#delete existing secret
+kubectl delete secret website-secret --namespace=dev
+#VSTS make sure outputformat is empty
+
+#create secret from file
+kubectl create secret generic website-secret --from-file=./Website/secret-frontend.json --namespace=dev
+
+#create / update config map - inline json
+kubectl apply -f ./Website/configmap-website.yaml --namespace=dev
+
+#delete existing config map external xml
+kubectl delete configmap config-xml-website --namespace=dev
+#VSTS make sure outputformat is empty
+
+#create config map external xml
+kubectl create configmap config-xml-website --from-file=./Website/configmap-frontend.xml --namespace=dev
+
+#create / update application
+kubectl apply -f ./Website/deployment-website.yaml --namespace=dev
+
+#scale 
+kubectl scale deployment frontend-deployment --replicas=2
+
+#view service
+IPAddress
+my-svc.my-namespace.svc.cluster.local
+e.g. http://frontend-service.dev.55474723f3b7466899bc.westeurope.aksapp.io
+http://dev-frontend-service.55474723f3b7466899bc.westeurope.aksapp.io
+
+#clear namespace
+kubectl delete daemonsets,replicasets,services,deployments,pods,secret,configmaps,rc --namespace=dev --all
+
+
