b38373a0b0b2621dce09dd2f0ab6a36b63f87225
Steve Leonard <steve-leonard@outlook.com>
2019-02-07 09:24:58 +0000

    API response flash

--- Source/AKSWebsite/wwwroot/js/site.js
+++ Source/AKSWebsite/wwwroot/js/site.js
@@ -2,3 +2,13 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+$('.blink').each(function() {
+    var elem = $(this);
+    setInterval(function() {
+        if (elem.css('visibility') == 'hidden') {
+            elem.css('visibility', 'visible');
+        } else {
+            elem.css('visibility', 'hidden');
+        }    
+    }, 1000);
+});
