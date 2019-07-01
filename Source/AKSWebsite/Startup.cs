5af97c92057acb373a1d15b70d384686226c8a01
Steve Leonard <steve-leonard@outlook.com>
2019-07-01 15:24:23 +0100

    "X-Original-URI"

--- Source/AKSWebsite/Startup.cs
+++ Source/AKSWebsite/Startup.cs
@@ -31,10 +31,12 @@ namespace AKSWebsite
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            //services.Configure<ForwardedHeadersOptions>(options =>
-            //{
-            //    options.ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
-            //});
+            
+            services.Configure<ForwardedHeadersOptions>(options =>
+            {
+                options.ForwardedForHeaderName = "X-Original-URI";
+                //options.ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
+            });
             services.Configure<CookiePolicyOptions>(options =>
             {
                 // This lambda determines whether user consent for non-essential cookies is needed for a given request.
@@ -58,9 +60,10 @@ namespace AKSWebsite
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            
             app.UseForwardedHeaders(new ForwardedHeadersOptions
             {
-                ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+                ForwardedForHeaderName = "X-Original-URI"
             });
             app.UseDeveloperExceptionPage(); //We are leaving this on for this demo to help trace any errors
             app.UseStaticFiles();
